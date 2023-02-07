7c013bc1b6a49c43714600cd58b969346ac7badb
hindtag <mbnvargas>
2023-02-07 11:41:08 +0800

    D-26: ebook activity

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,9 @@
-var article = "a";
-var numShirts = 0;
-var smallShirts = 0;
-var medShirts = 0;
-var largeShirts = 0;
+function sayHi() {
+    var txtName = document.getElementById("txtName");
+    divOutput = document.getElementById("divOutput")
 
-function calcShirts(sizeShirt) {
-    if (sizeShirt === "S" || sizeShirt === "s") {
-        smallShirts++;
-        numShirts++;
-    } else if (sizeShirt === "M" || sizeShirt === "m") {
-        medShirts++;
-        numShirts++;
-    } else if (sizeShirt === "L" || sizeShirt === "l") {
-        largeShirts++;
-        numShirts++;
-    }
+    name = txtName.value;
+
+    divOutput.innerHTML = " " + name + " "
+    divOutput.innerHTML += " is a very nice name."
 }
-
-do {
-    var answer = prompt("Would you like to purchase " + article + " t-shirt? If so, enter the size (S, M, L) and click OK. When you finish, click Cancel.",);
-
-    article = "ANOTHER";
-
-    if (answer !== null) {
-        calcShirts(answer);
-    }
-} while (answer !== null);
-
-document.write("You ordered " + numShirts + " shirts: " + smallShirts + " small, " + medShirts + " medium, " + largeShirts + " large.");
