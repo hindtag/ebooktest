733dd776ea7327e9b311307d5ec2e267d4f6eac1
hindtag <mbnvargas>
2023-02-08 14:36:17 +0800

    D-26: SOD

--- js/javascript.js
+++
@@ -1,9 +0,0 @@
-function sayHi() {
-    var txtName = document.getElementById("txtName");
-    divOutput = document.getElementById("divOutput")
-
-    name = txtName.value;
-
-    divOutput.innerHTML = " " + name + " "
-    divOutput.innerHTML += " is a very nice name."
-}
