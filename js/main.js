2d701f660758f96a524b38774087fdcbda363cb7
hindtag <mbnvargas>
2023-02-08 15:55:26 +0800

    Day 27: pinoyFreeCoder Object

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// || DATA TYPES
+// string, numbers, boolean, null, undefined
+
+// const Name = "Pedro";
+// const Age = 19;
+// const Rating = 49.3;
+// const isComplete = true;
+// const val = null;
+// const unk = undefined;
+
+// console.log(typeof val);
+
+// Explanation: By using typeof(argument), you can identify what data types of the variable.
+
+/*
+// || STRING MANIPULATION using concatenation and backticks
+const Name = "Pedro";
+const Age = 19;
+
+let string =` My name is ${Name} and my age is ${Age}`;
+
+// Explanation: Using template literals, the output of the console log is "My name is Pedro and my age is 19"
+
+// || Getting the length of a string
+console.log(Name.length); // Output is 5 for variable Named "Pedro"
+console.log(Name.toLocaleUpperCase()); // Output is PEDRO
+
+// .length is a dot notation method.
+// .toLocaleUpperCase() is also a built in method that capitalize the string.
+
+*/
+
+/*
+// || ARRAY
+
+const Fruits = ["Orange", "Mango", "Banana", "Grapes", "Pineapple"];
+
+Fruits[5] = "Santol"; // If you know the numbers of array, you can add "Santol" at the end of an array using this method.
+Fruits.push("Jack Fruit"); // If you don't know the numbers of an array.
+Fruits.unshift("Kamias")// You can add a new value at the start of an array.
+Fruits.pop();// This will remove the last value 
+
+console.log(Fruits); // Output is ["Kamias, Orange", "Mango", "Banana", "Grapes", "Pineapple, Santol", Jack Fruit]
+console.log(Fruits[1]); // Output is "Orange", because of the index since Kamias was added at the start.
+console.log(Array.isArray(Fruits)); // To check if it is an array.
+console.log(Fruits.indexOf("Banana")); // To check the index of a value
+
+Fruits.splice(0, 2); // With this method, kamias and orange was removed.
+console.log(Fruits);
+
+*/
+
+// || OBJECT literals
+// used for api
+
+const tao {
+    
+};
