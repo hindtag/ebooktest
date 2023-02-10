cb87c789ea019199622968b5f399552692a09c5c
hindtag <mbnvargas>
2023-02-10 13:55:12 +0800

    Day 29 css+javascript done

---
+++ repo/CountingForwardBackwardByFive.js
@@ -0,0 +1,26 @@
+var output;
+
+function init() {
+    output = document.getElementById("output");
+};
+
+function count() {
+    output.innerHTML = "";
+    for (i = 1; i <=10; i++){
+        output.innerHTML += i + "<br />"
+    }
+}
+
+function back() {
+    output.innerHTML = "";
+    for (i = 10; i > 0; i--){
+        output.innerHTML += i + "<br />"
+    }
+}
+
+function byFive() {
+    output.innerHTML = "";
+    for (i = 5; i <=50; i += 5){
+        output.innerHTML += i + "<br />"
+    }
+}
