9783125879aa11dbc9dce31b1c3f320a556ecddd
hindtag <mbnvargas>
2023-02-10 17:00:47 +0800

    Day 29: ebooktest javascript quick code
    change theme and move text

--- js/main2.js
+++ js/main2.js
@@ -1,18 +1,18 @@
-function displayText() {
-
-let firstGraf =document.getElementById("graf1");
-let secondGraf =document.getElementById("graf2");
-
-firstGraf.style.visibility = (document.myForm.graf1box.checked) ? "visible" : "hidden";
-secondGraf.style.visibility = (document.myForm.graf2box.checked) ? "visible" : "hidden";
-}
-
-function changeText(){
-
-let newText = document.myForm.changeableText.value;
-
-let oldText = document.getElementById("graf3")
-
-oldText.firstChild.nodeValue = newText;
-
+function move(direction) {
+    var layerText = document.getElementById("myLayer");
+    
+    switch (direction) {
+        case 'left':
+            layerText.style.left = 50;
+            break;
+        case 'right':
+            layerText.style.left = 150;
+            break;
+        case 'up':
+            layerText.style.top = 50;
+            break;
+        case 'down':
+            layerText.style.top = 150;
+            break;
+    }
 }
