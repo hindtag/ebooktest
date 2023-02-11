268dfd502a4930f7c8b1fc746883a8a534365d97
hindtag <mbnvargas>
2023-02-11 16:32:22 +0800

    Day 30: Done with assignment

--- js/pattern.js
+++ js/pattern.js
@@ -1,1 +1,38 @@
-for(let i = 0; i < 10; i++);
+//Note:
+//internal loop is row
+//external loop is column
+
+//square:
+// let n = 5;
+// let string = "";
+
+// for (let i = 0; i < n; i++){
+//     for (let j = 0; j < n; j++){
+//         string += "*";
+//     }
+//         string += "\n";
+// }
+
+// console.log(string); // Square using external and 
+
+// Hallow box
+let n = 5;
+let string ="";
+
+for (let i = 0; i < n; i++){
+    for (let j =0; j < n; j++){
+        if(i === 0 || i === n - 1){
+            string += "*";
+        }
+        else {
+            if (j === 0 || j === n - 1) {
+                string += "*";
+            }
+            else{
+                string += " ";
+            }
+        }
+    } string += "\n"
+}
+
+console.log(string);
